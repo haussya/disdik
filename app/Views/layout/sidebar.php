6d4b97905c7dcbc94650e2df468c8090c0c1555a
RadityaR <81510664+RadityaR@users.noreply.github.com>
2022-09-13 09:44:28 +0800

    nambah sidebar sama sidebar css

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,8 +1,43 @@
-<div class="sidebar close">
-    <div class="logo-details">
-        <img src= "<?php echo base_url("/asset/disdik.png");?>" width="159px" alt="">
-    </div>
-    <ul class="nav-links">
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?php echo base_url("/css/sidebar.css");?>">
+
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="header">
+            <div class="logo-details">
+                <img src="<?php echo base_url("/asset/disdik.png");?>" width="159px" alt="">
+            </div>
+        </div>
+        <div class="content">
+            <ul class="nav flex-column">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- <ul class="nav-links">
         <li>
             <a href="<?= base_url('/') ?>">
                 <i class='bx bxs-home-alt-2'></i>
@@ -36,4 +71,10 @@
             </a>
         </li>
     </ul>
-</div>
+</div> -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+        </script>
+</body>
+
+</html>
