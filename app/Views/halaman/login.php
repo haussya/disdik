43f9986e0dcd187bfb649b9465ffa822f8bf8fe7
Muhammad Nouval Rahman <80933907+haussya@users.noreply.github.com>
2022-09-14 11:37:02 +0800

    login

--- app/Views/halaman/login.php
+++ app/Views/halaman/login.php
@@ -1,98 +1,79 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Login - Sistem Informasi Data Sekolah Dinas Pendidikan Kota Banjarbaru</title>
-    <style>
-        div.box{
-            position: absolute;
-            width: 35%;
-            height: 100vh;
-            right: 0px;
-            top: 0px;   
-            background: linear-gradient(160.63deg, rgba(1, 146, 213, 0) 0%, #0192D5 78.55%);
-        }
-        .box1{
-            padding-top: 250px;
-            padding-bottom: 250px;
-            padding-left: 50px;
-            padding-right: 50px;
-        }
-        .logo{
-          position: absolute;  
-          left: 30px;
-          top: 30px;
-        }
-        .tombol {
-          padding-left: 40%;
-        }
-        .org{
-          padding-left: 40%;
-          padding-bottom: 15px;
-        }
-        body{
-          display: flex;  
-        }
-        .label{
-          color: white;
-        }
-        .imgbg{
-          height: 100vh;
-          width: 100%;
-        }
-    </style>
-  </head>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  <body>
-    <img class="imgbg" src="<?php echo base_url("public/asset/foto_login.png");?>">
-    <div >
-      <div class="logo">
-        <img class="imgdisdik" src="<?php echo base_url("public/asset/logo_disdik_rounded.png");?>" width="150px">
+  <title>Login - Sistem Informasi Data Sekolah Dinas Pendidikan Kota Banjarbaru</title>
+
+  <!-- CSS -->
+  <link rel="stylesheet" href="<?php echo base_url("public/css/login.css"); ?>">
+</head>
+
+<body>
+  <img class="imgbg" src="<?php echo base_url("public/asset/foto_login.png"); ?>">
+  <div>
+    <div class="logo">
+      <img class="imgdisdik" src="<?php echo base_url("public/asset/logo_disdik_rounded.png"); ?>" width="150px">
+      <link rel="stylesheet" href="">
+    </div>
+  </div>
+  <div class="box">
+    <div class="box1">
+      <div class="org">
+        <img class="imgorg-center" src="<?php echo base_url("public/asset/orgx.png"); ?>" width="80px">
         <link rel="stylesheet" href="">
       </div>
-    </div>
-    <div class="box">
-      <div class="box1">
-        <div class="org">
-          <img class="imgorg-center" src="<?php echo base_url("public/asset/orgx.png");?>" width="80px">
-          <link rel="stylesheet" href="">
+
+      <form method="POST" action="<?= base_url('/auth/process') ?>">
+        <?= csrf_field() ?>
+        <div class="mb-3">
+          <label for="exampleInputUsername1" class="form-label">Username</label>
+          <input type="username" class="form-control" id="exampleInputUsername1" name="username">
         </div>
-
-        <label class="label" for="password">User ID</label>
-
-        <div class="input-group mb-3">
-          <!-- <span class="input-group-text" id="basic-addon1" >User ID</span> -->
-          <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>        
-
-        <label class="label" for="password">Password</label>
-
-        <div class="input-group mb-3">
-          <!-- <span class="input-group-text" id="basic-addon1">Password</span> -->
-          <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input type="password" class="form-control" id="exampleInputPassword1" name="password">
         </div>
-
         <div class="tombol">
-          <button type="button" class="btn btn-light">Login</button>
+          <button type="submit" class="btn btn-light">
+            Login
+          </button>
         </div>
-    </div>
-    
-    
-    <!-- Optional JavaScript; choose one of the two! -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+        <!-- <label class="label" for="password">User ID</label>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1" >User ID</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+      </div>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
+      <label class="label" for="password">Password</label>
+      <div class="input-group mb-3">
+        <span class="input-group-text" id="basic-addon1">Password</span>
+        <input type="text" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+      </div>
+
+      <div class="tombol">
+        <button type="button" class="btn btn-light">Login</button>
+      </div>
+    </div> -->
+
+
+        <!-- Optional JavaScript; choose one of the two! -->
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
-  </body>
+</body>
+
 </html>
