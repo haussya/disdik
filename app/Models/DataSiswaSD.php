d91095a76ce1df9fd2f4886b96b3cc240147c0fd
Muhammad Nouval Rahman <80933907+haussya@users.noreply.github.com>
2022-09-27 11:45:25 +0800

    model siswa

--- app/Models/DataSiswaSD.php
+++ app/Models/DataSiswaSD.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Models;
+
 use CodeIgniter\Model;
 
 class DataSiswaSD extends Model
 {
     protected $table = 'data_siswa_sd';
     protected $primaryKey = 'nisn';
-    protected $allowedFields = ['nama', 'kelamin', 'tanggal_lahir', 'tingkat', 'domisili', 'nama_ibu', 'status'. 'keterangan'];
+    protected $allowedFields = ['nama', 'kelamin', 'tanggal_lahir', 'tingkat', 'domisili', 'nama_ibu', 'status' . 'keterangan'];
 
     public function hitungDataSiswaSD()
     {
-        return $this->db->table('data_siswa_sd')->countAll();
+        return $this->db->table('data_siswa_sd')
+            ->join('domisili', 'domisili.domisili_id=data_siswa_sd.domisili_id')
+            ->get()->getResultArray();
     }
 }
--- app/Models/DataSiswaSMP.php
+++ app/Models/DataSiswaSMP.php
@@ -1,16 +1,19 @@
 <?php
 
 namespace App\Models;
+
 use CodeIgniter\Model;
 
 class DataSiswaSMP extends Model
 {
     protected $table = 'data_siswa_smp';
     protected $primaryKey = 'nisn';
-    protected $allowedFields = ['nama', 'kelamin', 'tingkat', 'domisili'];
+    protected $allowedFields = ['nama', 'kelamin', 'tanggal_lahir', 'tingkat', 'domisili', 'nama_ibu', 'status' . 'keterangan'];
 
     public function hitungDataSiswaSMP()
     {
-        return $this->db->table('data_siswa_smp')->countAll();
+        return $this->db->table('data_siswa_smp')
+            ->join('domisili', 'domisili.domisili_id=data_siswa_smp.domisili_id')
+            ->get()->getResultArray();
     }
 }
