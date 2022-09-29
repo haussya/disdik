de7ae681067bfde42f51750bcde82b4aca148600
RadityaR <81510664+RadityaR@users.noreply.github.com>
2022-09-29 09:03:43 +0800

    gygg

--- app/Views/halaman/HalamanTabelSiswa.php
+++ app/Views/halaman/HalamanTabelSiswa.php
@@ -7,112 +7,127 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <title>Data Siswa - Sistem Informasi Data Sekolah Dinas Pendidikan Kota Banjarbaru</title>
 
     <!-- CSS -->
 
     <style>
-        * {
-            font-family: 'Poppins', sans-serif;
-        }
+    * {
+        font-family: 'Poppins', sans-serif;
+    }
 
-        body {
-            position: relative;
-            min-height: 100vh;
-            display: flex;
-            justify-content: flex-end;
-        }
+    body {
+        position: relative;
+        min-height: 100vh;
+        display: flex;
+        justify-content: flex-end;
+    }
 
-        nav {
-            display: flex;
-            justify-content: flex-end;
-            height: 50px;
-            width: 100vw;
-            min-width: 100vw;
-            background: #ffffff;
-            box-shadow: 0px 12px 6px -3px rgba(0, 0, 0, 0.25);
-            position: absolute;
-            right: 0;
-        }
+    nav {
+        display: flex;
+        justify-content: flex-end;
+        height: 50px;
+        width: 100vw;
+        min-width: 100vw;
+        background: #ffffff;
+        box-shadow: 0px 12px 6px -3px rgba(0, 0, 0, 0.25);
+        position: absolute;
+        right: 0;
+    }
 
-        .imgbg {
-            display: flex;
-            justify-content: space-between 20;
-            height: 100vh;
-            width: 100%;
-        }
+    .imgbg {
+        display: flex;
+        justify-content: space-between 20;
+        height: 100vh;
+        width: 100%;
+    }
 
-        .org {
-            padding-top: 12px;
-            padding-right: 12px;
-            display: flex-end;
+    .org {
+        padding-top: 12px;
+        padding-right: 12px;
+        display: flex-end;
 
-        }
+    }
 
-        .orgx {
+    .orgx {
 
-            padding-right: 15px;
-            padding-top: 12px;
-            padding-bottom: 25px;
-            border: 1px;
-        }
+        padding-right: 15px;
+        padding-top: 12px;
+        padding-bottom: 25px;
+        border: 1px;
+    }
 
-        p {
-            font-size: 12px;
-            font-weight: bold;
-            padding-top: 7px;
-            padding-right: 20px;
-            color: #5A5B5A;
-        }
+    p {
+        font-size: 12px;
+        font-weight: bold;
+        padding-top: 7px;
+        padding-right: 20px;
+        color: #5A5B5A;
+    }
 
-        .a {
-            position: fixed;
-       
-            height: 75%;
-            margin-top: 9%;
-            margin-left: 5%;
-            padding-left: 124px;
-
-         
-        }
+    .a {
+        position: fixed;
+        height: 75%;
+        margin-top: 9%;
+        margin-left: 5%;
+        padding-left: 124px;
 
 
-        .headline {
-            font-size: 22px;
-            font-weight: bold;
-            padding-top: 20px;
-            padding-left: 20px;
-            padding-right: 20px;
-            padding-bottom: 5px;
-        }
+    }
 
-        .bo {
-            padding-left: 20px;
-            padding-right: 20px;
-        }
 
-        .ddsekolah {
-            position: absolute;
-            width: 300px;
-            height: 30px;
-            margin-top: 5%;
-            margin-left: 5%;
+    .headline {
+        font-size: 22px;
+        font-weight: bold;
+        padding-top: 20px;
+        padding-left: 20px;
+        padding-right: 20px;
+        padding-bottom: 5px;
+    }
 
-            background-color: #ffffff;
-            box-shadow: 0px 12px 6px -3px rgba(0, 0, 0, 0.2);
-        }
+    .bo {
+        padding-left: 20px;
+        padding-right: 20px;
+    }
 
-        .tombol {
-            display: flexbox;
-            flex-direction: column-reverse;
-            position: absolute;
-            height: 30px;
-            margin-top: 5%;
-            margin-left: 45%;
-            /* box-shadow: 0px 12px 6px -3px rgba(0, 0, 0, 0.2); */
-        }
+    .ddsekolah {
+        position: absolute;
+        width: 300px;
+        height: 30px;
+        margin-top: 5%;
+        margin-left: 5%;
+
+        background-color: #ffffff;
+        box-shadow: 0px 12px 6px -3px rgba(0, 0, 0, 0.2);
+    }
+
+    .tombol {
+        display: flexbox;
+        flex-direction: column-reverse;
+        position: absolute;
+        height: 30px;
+        margin-top: 5%;
+        margin-left: 45%;
+        /* box-shadow: 0px 12px 6px -3px rgba(0, 0, 0, 0.2); */
+    }
+
+    .wrapper {
+        display: flex;
+        height: 100vh;
+        overflow: hidden;
+    }
+
+    .content {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        flex: 1 1 0%;
+        overflow-y: auto;
+        overflow-x: hidden;
+    }
     </style>
 
 
@@ -122,10 +137,9 @@
 </head>
 
 <body>
-    <?= $this->include('layout/sidebar'); ?>
+    <div class="wrapper">
 
-    <div class="content">
-
+        <?= $this->include('layout/sidebar'); ?>
         <div class="ddsekolah">
             <select class="form-select" aria-label="Default select example">
                 <option selected dis>Sekolah</option>
@@ -137,32 +151,36 @@
                 <option value="SDN3LUB">SD NEGERI 3 LANDASAN ULIN BARAT</option>
             </select>
         </div>
-        <div class="tombol">
-            <a href="http://localhost/disdik/public/input" class="btn btn-primary">Tambah Data</a>
-        </div>
 
-        <div class="a">
-            <p class="headline">TABEL DATA SISWA</p>
-            <div class="bo">
-                <table class="table table-info">
-                    <thead>
-                        <tr>
-                            <th scope="col">No.</th>
-                            <th scope="col">NISN</th>
-                            <th scope="col">Nama</th>
-                            <th scope="col">Kelamin</th>
-                            <th scope="col">Tanggal Lahir</th>
-                            <th scope="col">Tingkatan</th>
-                            <th scope="col">Domisili</th>
-                            <th scope="col">nama Orangtua</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Keterangan</th>
-                            <th scope="col">Edit</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $no = 1 ?>
-                        <?php foreach ($dataSiswaSD as $sd) : ?>
+        <div class="content">
+
+
+            <div class="tombol">
+                <a href="http://localhost/disdik/public/input" class="btn btn-primary">Tambah Data</a>
+            </div>
+
+            <div class="a">
+                <p class="headline">TABEL DATA SISWA</p>
+                <div class="bo">
+                    <table class="table table-info">
+                        <thead>
+                            <tr>
+                                <th scope="col">No.</th>
+                                <th scope="col">NISN</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Kelamin</th>
+                                <th scope="col">Tanggal Lahir</th>
+                                <th scope="col">Tingkatan</th>
+                                <th scope="col">Domisili</th>
+                                <th scope="col">nama Orangtua</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Keterangan</th>
+                                <th scope="col">Edit</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 1 ?>
+                            <?php foreach ($dataSiswaSD as $sd) : ?>
                             <tr>
                                 <td><?= $no ?></td>
                                 <td><?= $sd['nisn']; ?></td>
@@ -179,18 +197,23 @@
                                 </td>
                             </tr>
                             <?php $no++ ?>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
+
+
     <img class="imgbg" src="<?php echo base_url("public/asset/foto_dashboard.png"); ?>">
 </body>
 <!-- Optional JavaScript; choose one of the two! -->
 
 <!-- Option 1: Bootstrap Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+</script>
 
 <!-- Option 2: Separate Popper and Bootstrap JS -->
 <!--
