cdea0a5de9cbfc3a5e1bf5f50674f68670253bf2
Infamous0192 <infamous0192@gmail.com>
2022-09-29 19:42:41 +0800

    minor revision

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -7,20 +7,20 @@
   <div class="kartuall d-flex justify-content-between">
     <div class="kartusiswa">
       <div class="fotokartu">
-        <img class="imgcard-center" src="<?php echo base_url("/asset/gambar_siswa.png"); ?>">
+        <img class="imgcard-center" src="<?= base_url("/asset/gambar_siswa.png"); ?>">
       </div>
       <div class="text">
         <p class="tulisankartu">SISWA</p>
-        <a class="link" href="http://localhost/disdik//InputSiswaSD">Lihat Lebih Lanjut</a>
+        <a class="link" href="<?= base_url("/InputSiswaSD"); ?>">Lihat Lebih Lanjut</a>
       </div>
     </div>
     <div class="kartuguru">
       <div class="fotokartu">
-        <img class="imgcard-center" src="<?php echo base_url("/asset/gambar_guru.png"); ?>">
+        <img class="imgcard-center" src="<?= base_url("/asset/gambar_guru.png"); ?>">
       </div>
       <div class="text">
         <p class="tulisankartu">GURU</p>
-        <a class="link" href="http://localhost/disdik//tabelguru">Lihat Lebih Lanjut</a>
+        <a class="link" href="<?= base_url("/tabelguru"); ?>">Lihat Lebih Lanjut</a>
       </div>
     </div>
     <div class="kartusarpras">
