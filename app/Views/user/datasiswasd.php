3cc9f80488a26685db808f9a7c9238d900e08555
Muhammad Nouval Rahman <80933907+haussya@users.noreply.github.com>
2022-10-17 10:20:51 +0800

    new

--- app/Views/user/datasiswasd.php
+++ app/Views/user/datasiswasd.php
@@ -4,8 +4,16 @@
 <div class="page-heading">
     <div class="page-title">
         <div class="row">
-            <div class="d-flex justify-content-between">
+            <div class="col-12 col-md-6 order-md-1 order-last">
                 <h3>Siswa</h3>
+            </div>
+            <div class="col-12 col-md-6 order-md-2 order-first">
+                <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/user">Dashboard</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Data Siswa</li>
+                    </ol>
+                </nav>
             </div>
             <?php if (session()->getFlashdata('pesan')) : ?>
                 <div class="alert alert-success" role="alert">
@@ -18,9 +26,9 @@
         <div class="card">
             <div class="card-header d-flex align-items-center justify-content-between">
                 <h4>Data Siswa</h4>
-                <a href="/user/siswasd/tambah">
+                <a href="/user/datasiswasd/tambah">
                     <button class="btn btn-primary px-3">
-                        Add
+                        Tambah
                     </button>
                 </a>
             </div>
