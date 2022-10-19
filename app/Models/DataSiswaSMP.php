39d9096bef1f86513a4993732c4ec940587172c0
Muhammad Nouval Rahman <80933907+haussya@users.noreply.github.com>
2022-10-19 10:40:18 +0800

    p

--- app/Models/DataSiswaSMP.php
+++ app/Models/DataSiswaSMP.php
@@ -11,7 +11,7 @@ class DataSiswaSmp extends Model
     protected $primaryKey = 'nisn';
     protected $allowedFields = ['nisn', 'nama', 'kelamin_id', 'tanggal_lahir', 'tingkat_id', 'domisili_id', 'nama_ibu', 'status_id'];
 
-    public function getSiswaSD()
+    public function getSiswaSMP()
     {
         return $this->db->table('data_siswa_smp')
             ->join('kelamin', 'kelamin.kelamin_id=data_siswa_smp.kelamin_id')
