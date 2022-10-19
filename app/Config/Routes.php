d80c9bed75f71e980b9e77918b08dd54397acc0e
Muhammad Nouval Rahman <80933907+haussya@users.noreply.github.com>
2022-10-19 12:35:18 +0800

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -48,6 +48,69 @@ $routes->group('admin', ['namespace' => 'App\Controllers\Admin'], function ($rou
         $routes->post('update/(:num)', 'Siswasd::update/$1');
         $routes->delete('hapus/(:num)', 'Siswasd::hapus/$1');
     });
+
+    $routes->group('datasiswasmp', function ($routes) {
+        $routes->get('/', 'Siswasmp::index');
+        $routes->get('edit/(:num)', 'Siswasmp::edit/$1');
+        $routes->post('update/(:num)', 'Siswasmp::update/$1');
+        $routes->delete('hapus/(:num)', 'Siswasmp::hapus/$1');
+    });
+
+    $routes->group('beasiswasd', function ($routes) {
+        $routes->get('/', 'Beasiswasd::index');
+        $routes->get('edit/(:num)', 'Beasiswasd::edit/$1');
+        $routes->post('update/(:num)', 'Beasiswasd::update/$1');
+        $routes->delete('hapus/(:num)', 'Beasiswasd::hapus/$1');
+    });
+
+    $routes->group('beasiswasmp', function ($routes) {
+        $routes->get('/', 'Beasiswasmp::index');
+        $routes->get('edit/(:num)', 'Beasiswasmp::edit/$1');
+        $routes->post('update/(:num)', 'Beasiswasmp::update/$1');
+        $routes->delete('hapus/(:num)', 'Beasiswasmp::hapus/$1');
+    });
+
+    $routes->group('doltmsd', function ($routes) {
+        $routes->get('/', 'Doltmsd::index');
+        $routes->get('edit/(:num)', 'Doltmsd::edit/$1');
+        $routes->post('update/(:num)', 'Doltmsd::update/$1');
+        $routes->delete('hapus/(:num)', 'Doltmsd::hapus/$1');
+    });
+
+    $routes->group('doltmsmp', function ($routes) {
+        $routes->get('/', 'Doltmsmp::index');
+        $routes->get('edit/(:num)', 'Doltmsmp::edit/$1');
+        $routes->post('update/(:num)', 'Doltmsmp::update/$1');
+        $routes->delete('hapus/(:num)', 'Doltmsmp::hapus/$1');
+    });
+
+    $routes->group('gurusd', function ($routes) {
+        $routes->get('/', 'Gurusd::index');
+        $routes->get('edit/(:num)', 'Gurusd::edit/$1');
+        $routes->post('update/(:num)', 'Gurusd::update/$1');
+        $routes->delete('hapus/(:num)', 'Gurusd::hapus/$1');
+    });
+
+    $routes->group('gurusmp', function ($routes) {
+        $routes->get('/', 'Gurusmp::index');
+        $routes->get('edit/(:num)', 'Gurusmp::edit/$1');
+        $routes->post('update/(:num)', 'Gurusmp::update/$1');
+        $routes->delete('hapus/(:num)', 'Gurusmp::hapus/$1');
+    });
+
+    $routes->group('sarprassd', function ($routes) {
+        $routes->get('/', 'Sarprassd::index');
+        $routes->get('edit/(:num)', 'Sarprassd::edit/$1');
+        $routes->post('update/(:num)', 'Sarprassd::update/$1');
+        $routes->delete('hapus/(:num)', 'Sarprassd::hapus/$1');
+    });
+
+    $routes->group('sarprassmp', function ($routes) {
+        $routes->get('/', 'Sarprassmp::index');
+        $routes->get('edit/(:num)', 'Sarprassmp::edit/$1');
+        $routes->post('update/(:num)', 'Sarprassmp::update/$1');
+        $routes->delete('hapus/(:num)', 'Sarprassmp::hapus/$1');
+    });
 });
 
 $routes->group('user', ['namespace' => 'App\Controllers\User'], function ($routes) {
@@ -57,6 +120,60 @@ $routes->group('user', ['namespace' => 'App\Controllers\User'], function ($route
         $routes->get('/', 'Siswasd::index');
         $routes->get('tambah', 'Siswasd::tambah');
         $routes->post('simpan', 'Siswasd::simpan');
+    });
+
+    $routes->group('datasiswasmp', function ($routes) {
+        $routes->get('/', 'Siswasmp::index');
+        $routes->get('tambah', 'Siswasmp::tambah');
+        $routes->post('simpan', 'Siswasmp::simpan');
+    });
+
+    $routes->group('beasiswasd', function ($routes) {
+        $routes->get('/', 'Beasiswasd::index');
+        $routes->get('tambah', 'Beasiswasd::tambah');
+        $routes->post('simpan', 'Beasiswasd::simpan');
+    });
+
+    $routes->group('beasiswasmp', function ($routes) {
+        $routes->get('/', 'Beasiswasmp::index');
+        $routes->get('tambah', 'Beasiswasmp::tambah');
+        $routes->post('simpan', 'Beasiswasmp::simpan');
+    });
+
+    $routes->group('doltmsd', function ($routes) {
+        $routes->get('/', 'Doltmsd::index');
+        $routes->get('tambah', 'Doltmsd::tambah');
+        $routes->post('simpan', 'Doltmsd::simpan');
+    });
+
+    $routes->group('doltmsmp', function ($routes) {
+        $routes->get('/', 'Doltmsmp::index');
+        $routes->get('tambah', 'Doltmsmp::tambah');
+        $routes->post('simpan', 'Doltmsmp::simpan');
+    });
+
+    $routes->group('gurusd', function ($routes) {
+        $routes->get('/', 'Gurusd::index');
+        $routes->get('tambah', 'Gurusd::tambah');
+        $routes->post('simpan', 'Gurusd::simpan');
+    });
+
+    $routes->group('gurusmp', function ($routes) {
+        $routes->get('/', 'Gurusmp::index');
+        $routes->get('tambah', 'Gurusmp::tambah');
+        $routes->post('simpan', 'Gurusmp::simpan');
+    });
+
+    $routes->group('sarprassd', function ($routes) {
+        $routes->get('/', 'Sarprassd::index');
+        $routes->get('tambah', 'Sarprassd::tambah');
+        $routes->post('simpan', 'Sarprassd::simpan');
+    });
+
+    $routes->group('sarprassmp', function ($routes) {
+        $routes->get('/', 'Sarprassmp::index');
+        $routes->get('tambah', 'Sarprassmp::tambah');
+        $routes->post('simpan', 'Sarprassmp::simpan');
     });
 });
 
